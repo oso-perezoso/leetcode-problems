2f16a1e314ee161a8a2fc5b1e3722c300bbbded2
oso-perezoso <alexey.beshenov@yandex.ru>
2022-01-21 23:06:18 +0300

    A couple of two pointers problems

---
+++ rotate_array.py
@@ -0,0 +1,24 @@
+# Rotate Array
+# https://leetcode.com/problems/rotate-array/
+
+
+from typing import List
+
+
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        # Example: [1,2,3,4,5,6,7], k = 3
+        # Reverse: [7,6,5,4,3,2,1]
+        # Reverse from 0 to 2: [5,6,7,4,3,2,1]
+        # Reverse from 3 to 6: [5,6,7,1,2,3,4]
+
+        def reverse(nums: List[int], start: int, end: int) -> None:
+            while start < end:
+                nums[start], nums[end] = nums[end], nums[start]
+                start = start + 1
+                end = end - 1
+
+        k = k % len(nums)
+        reverse(nums, 0, len(nums) - 1)
+        reverse(nums, 0, k - 1)
+        reverse(nums, k, len(nums) - 1)
---
+++ squares_of_a_sorted_array.py
@@ -0,0 +1,37 @@
+# Squares of a Sorted Array
+# https://leetcode.com/problems/squares-of-a-sorted-array/
+
+
+from typing import List
+
+
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        result: List[int] = []
+
+        i: int = 0
+        while i < len(nums) and nums[i] < 0:
+            i = i + 1
+        # now i points to the first positive element
+
+        j: int = i - 1  # j points to the last negative element
+
+        while i < len(nums) and j >= 0:
+            if nums[i] <= -nums[j]:
+                result.append(nums[i] ** 2)
+                i = i + 1
+            else:
+                result.append(nums[j] ** 2)
+                j = j - 1
+
+        # If i < len(nums) and j < 0:
+        while i < len(nums):
+            result.append(nums[i] ** 2)
+            i = i + 1
+
+        # If j >= 0 and i >= len(nums):
+        while j >= 0:
+            result.append(nums[j] ** 2)
+            j = j - 1
+
+        return result
