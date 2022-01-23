9cc9307af87aea9114a84911a36ea02ab79a1988
oso-perezoso <alexey.beshenov@yandex.ru>
2022-01-23 10:35:05 +0300

    Better count from 0

--- climbing_stairs.py
+++ climbing_stairs.py
@@ -8,8 +8,8 @@ class Solution:
         #   F_1 = 1, F_2 = 2, F_n = F_{n-1} + F_{n-1}
 
         # Memoization:
-        fib: list[int] = [1, 2]
-        while len(fib) < n:
+        fib: list[int] = [1, 1]
+        while len(fib) <= n:
             fib.append(fib[-1] + fib[-2])
 
-        return fib[n - 1]
+        return fib[n]
