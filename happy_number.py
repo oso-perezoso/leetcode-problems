49704bd68d33723c73e8951a1716daa7a9423307
oso-perezoso <alexey.beshenov@yandex.ru>
2022-01-24 10:20:13 +0300

    Happy Number

---
+++ happy_number.py
@@ -0,0 +1,26 @@
+# Happy Number
+# https://leetcode.com/problems/happy-number/
+
+
+from typing import Set
+
+
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        def _digit_squares(n: int) -> int:
+            square_sum: int = 0
+            while n != 0:
+                square_sum = square_sum + (n % 10) ** 2
+                n = n // 10
+            return square_sum
+
+        numbers: Set[int] = set([n])
+
+        while n != 1:
+            n = _digit_squares(n)
+            if n in numbers:  # loop detected
+                return False
+            else:
+                numbers.add(n)
+
+        return True
