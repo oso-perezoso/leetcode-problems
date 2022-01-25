be1fb4d906ddd7270e2a29582f4d907ec1328530
oso-perezoso <alexey.beshenov@yandex.ru>
2022-01-25 15:08:21 +0300

    3Sum

---
+++ 3sum.py
@@ -0,0 +1,39 @@
+# 3Sum
+# https://leetcode.com/problems/3sum/
+
+
+from typing import List
+
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        result: List[List[int]] = []
+
+        for i in range(len(nums) - 2):
+            j: int = i + 1
+            k: int = len(nums) - 1
+
+            if i > 0 and nums[i] == nums[i - 1]:  # we skip duplicates
+                continue
+
+            while j < k:
+                s: int = nums[i] + nums[j] + nums[k]
+
+                if s == 0:
+                    result.append([nums[i], nums[j], nums[k]])
+                    j, k = j + 1, k - 1
+                    while j < k and nums[j] == nums[j - 1]:
+                        j = j + 1
+                    while j < k and nums[k] == nums[k + 1]:
+                        k = k - 1
+                elif s < 0:
+                    j = j + 1
+                    while j < k and nums[j] == nums[j - 1]:
+                        j = j + 1
+                else:
+                    k = k - 1
+                    while j < k and nums[k] == nums[k + 1]:
+                        k = k - 1
+
+        return result
